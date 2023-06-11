43409f5d9e095265e4c3b5c0ac6133feafd7e221
Dhruv <prashardhruv2229@gmail.com>
2023-06-11 11:22:23 +0530

    ethereal for testing

---
+++ controllers/sendEmail.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+
+const sendEmail = async(req,res)=>{
+
+    let testAccount = await nodemailer.createTestAccount();
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'freeman.cormier@ethereal.email',
+            pass: 'uXuhPZk4wZXK36uZQS'
+        }
+    });
+    let info = await transporter.sendMail({
+        from :'"Coding addict"<codingaddict@gmail.com>',
+        to: "bar@example.com",
+        subject: "Hello",
+        html: '<h2>SENDING emails with nodejs</h2>'
+    });
+    res.json(info);
+
+    // res.send('send email func');
+}
+
+
+module.exports= sendEmail;
